1b8814584ea2a5d61540527c5baf0e75d15b923e
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2023-03-06 20:16:34 +0530

    Update technology.js

--- Routes/Top-Post/technology.js
+++ Routes/Top-Post/technology.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const technology=express.Router();
-const data=require('../../ApiControllers/Top-Post/technology');
+const data=require('../../ApiControllers/Top-Post/Technology');
 const page=technology.route('/technology/topPost');
 page.get(data.apiController);
 module.exports=technology;
