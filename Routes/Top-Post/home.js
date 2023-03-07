769be05cef360c1170f70996bd3d165afec6c095
TAMILARASAN S <111070542+tamil6268@users.noreply.github.com>
2023-03-07 14:30:00 +0530

    Update home.js

--- Routes/Top-Post/home.js
+++ Routes/Top-Post/home.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const home=express.Router();
-const data=require('../../ApiControllers/Top-Post/home');
+const data=require('../../ApiControllers/Top-Post/Home');
 const page=home.route('/home/topPost');
 page.get(data.apiController);
 module.exports=home;
